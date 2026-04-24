c93110deeaa35cd3bc9d0388dcd53e05046e6e81
Daniel Heimburger <daniel.heimburger@pointsharp.com>
2026-04-24 15:04:38 +0200

    Initialize backend
    # create spring-kotlin project

---
+++ backend/src/main/kotlin/com/danih24/lease_management/LeaseManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.danih24.lease_management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeaseManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<LeaseManagementApplication>(*args)
+}
